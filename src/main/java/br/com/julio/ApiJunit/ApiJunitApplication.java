0ccefe978644c143dabc750cdaaf7ec2426f3182
Julio Cesar <julio.cq96@gmail.com>
2022-04-12 15:55:41 -0300

    First Commit

---
+++ src/main/java/br/com/julio/ApiJunit/ApiJunitApplication.java
@@ -0,0 +1,13 @@
+package br.com.julio.ApiJunit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiJunitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiJunitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/julio/ApiJunit/ApiJunitApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.julio.ApiJunit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiJunitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
